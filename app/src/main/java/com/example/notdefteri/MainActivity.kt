20574ee3f2efdc6784292e1da16533fb5baa83b7
erayclk <92385578+erayclk@users.noreply.github.com>
2025-05-15 23:03:34 +0300

    add splash screen support and update theme configuration

--- app/src/main/java/com/example/notdefteri/MainActivity.kt
+++ app/src/main/java/com/example/notdefteri/MainActivity.kt
@@ -11,12 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.notdefteri.Screens.SplashScreen
 import com.example.notdefteri.ui.theme.NotDefteriTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
+
+
         setContent {
 
         }
